4f0c0bd5f6fc7d16f6fc3394fbab40ccf54f5cb4
Patrick Choi <mr_patrickchoi@yahoo.com>
2022-04-18 15:50:23 +0300

    making use of JSX, rendering dynamic content inside component with curly braces

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-const App = () => (
+const App = () => {
+  console.log("Hello from component")
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
   <div>
-    <p>Hello world</p>
+    <p>Hello world, it is {now.toString()}</p>
+    <p>
+      {a} plus {b} is {a + b}
+    </p>
   </div>
-)
+  )
+}
 
 export default App;
