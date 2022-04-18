b09fbaeae9c35ef439b7ab3f8903f844ff8539a6
Patrick Choi <mr_patrickchoi@yahoo.com>
2022-04-18 15:54:03 +0300

    Using multiple components, combining them, React philosophy of composing applications from many specialized reusable components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-const App = () => {
-  console.log("Hello from component")
-  const now = new Date()
-  const a = 10
-  const b = 20
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 
+const App = () => {
   return (
   <div>
-    <p>Hello world, it is {now.toString()}</p>
-    <p>
-      {a} plus {b} is {a + b}
-    </p>
+    <h1>Greetings</h1>
+    <Hello />
+    <Hello />
+    <Hello />
   </div>
   )
 }
